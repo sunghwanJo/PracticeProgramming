461c91e9938343fc9dfbf229d409bbbe18b583ef
sunghwanJo <namul10@gmail.com>
2013-03-12 11:17:04 +0900

    button 9

--- source.py
+++ source.py
@@ -3,6 +3,6 @@ from Tkinter import *
 def callback():
 	print 1
 
-button = Button(None, text='button', command='callback')
+button9 = Button(None, text='button', command='callback')
 button.pack()
 mainloop()
